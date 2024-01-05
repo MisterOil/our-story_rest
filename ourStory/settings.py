4ff381c0f6cfd7b8e6c3aed99a8eac892a18124f
MisterOil <molirun91@gmail.com>
2024-01-05 22:31:17 +0700

    fix

--- ourStory/settings.py
+++ ourStory/settings.py
@@ -160,5 +160,5 @@ if 'RENDER' not in os.environ:
     CORS_ALLOW_ALL_ORIGINS = True
     CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
 else:
-    ALLOWED_HOSTS =[os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
+    ALLOWED_HOSTS =[os.environ.get('RENDER_EXTERNAL_HOSTNAME'),'https://our-story-ljm9.onrender.com']
     CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('RENDER_EXTERNAL_HOSTNAME'),'https://our-story-ljm9.onrender.com/']
