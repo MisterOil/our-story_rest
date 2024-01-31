d9d9f70d8f7c6cca962069ae325d09d5f06023e7
MisterOil <molirun91@gmail.com>
2024-01-31 16:27:48 +0700

    update sty

--- ourStory/views.py
+++ ourStory/views.py
@@ -10,9 +10,11 @@ def story_list(request):
         return get_stories(request)
     elif request.method == 'POST':
         return create_story(request)
+    elif request.method == 'PUT':
+        return update_story(request)
 
 def get_stories(request):
-    stories = Story.objects.all()
+    stories = Story.objects.all().order_by('create_date')
     serializer = StorySerializers(stories, many=True)
     return JsonResponse({"stories": serializer.data}, safe=False)
 
@@ -22,4 +24,18 @@ def create_story(request):
     if serializer.is_valid():
         serializer.save()
         return JsonResponse(serializer.data, status=201)
+    return JsonResponse(serializer.errors, status=400)
+
+def update_story(request):
+    data = JSONParser().parse(request)
+    try:
+        story_id = data['id']
+        story = Story.objects.get(pk=story_id)
+    except (Story.DoesNotExist, KeyError):
+        return JsonResponse({"error": "Story not found"}, status=404)
+
+    serializer = StorySerializers(story, data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data)
     return JsonResponse(serializer.errors, status=400)
